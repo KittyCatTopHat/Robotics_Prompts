db70f0192124b4e790225c7c040cd6b955035534
KittyCatTopHat <irenek322@stemcollegiate.ca>
2025-02-10 13:23:11 -0700

    Create Feb10.py
    First day of 100 day challenge and practice.

---
+++ Feb10.py
@@ -0,0 +1,46 @@
+print("Hi Me")
+print("The totorial is telling me what to put.")
+print("I'm not doing that though. :>")
+print("I'm being original!")
+print("""The totorial is teaching me new stuff, like this triple quote. This can have loooooots of text and have symbols. Oooo, Ahhhhh. Very nice indeedy.""")
+print("""Wait... it says I can also do line brakes, let's try this!
+So I guess I'll just say something random now,
+and then I just Pressed enter... Let's see if that worked.""")
+print("It did!!! Yay!")
+
+print("Ok, so now we be practicing fixing errors. Let's go!")
+print("These next few lines of code are not going to me talking btw.")
+
+print("What could go wrong?")
+print ("Hello Again")
+print("Please work")
+
+print("It's me again, now we do even more error finding-ness. yay. :P")
+
+print(":= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+print("Done and done.")
+
+print("...Turns out I didn't need to do any of that. *sigh*")
+print("Here go with the actual thing I need to do...")
+
+print("So I need to write my name and the date. So, I'm Irene and today is Febuary 10 2025. We can all agree on that right? Right? Ok, good.")
+print("Oh wait, seperate lines of code. Let's try this again.")
+print("I'm Irene.")
+print("Today is the 10th of Febuary in 2025.")
+
+print("Now have to copy some text, not my own words! Just boring predetermined text.When I'm talking, you have no clue what I'm going to say next! Salamander. See, I bet you didn't see that one coming.")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+print("That is done now... moving on.")
+print("""I just read it, and of course I have an excuse to not do this in the middle of feild! What if I don't have my phone, or there's no service, or I just want enjoy the fact I'm in a feild and not rotting away infront of a screen?!?""")
+print("Ooo, now get share my feelings. I'm really making this drag on because I'm talking so much. I know I don't have to, but because I am, I'm getting really tired of this. Uuuuugh😫")
+
+print("Now I have to say that you can follow my progress at replit.com/@KittyCatTopHat.")
+print("Please don't.")
