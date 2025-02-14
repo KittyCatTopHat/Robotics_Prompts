78e18228e9a8f94ef8d1507b63b3ff84b43b4d3c
KittyCatTopHat <irenek322@stemcollegiate.ca>
2025-02-14 09:30:44 -0700

    Create Feb14.py

---
+++ Feb14.py
@@ -0,0 +1,7 @@
+#Today we learned how to print strings and variables in the same function
+food = input("food:")
+plant = input("plant:")
+cooking = input("cooking:")
+burned = input("burned:")
+item = input("item:")
+print("sprinkle",plant, "on", food, "and", cooking, "until", burned, "and serve on a", item)
