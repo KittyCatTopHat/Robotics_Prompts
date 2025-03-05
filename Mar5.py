fce5f38188fb0f40a35050c10f08582a5298d5e8
KittyCatTopHat <irenek322@stemcollegiate.ca>
2025-03-05 10:19:47 -0700

    Create Mar5.py

---
+++ Mar5.py
@@ -0,0 +1,16 @@
+# Today we learned about if/else statments and how to use them.
+familiar = input("Pick a familiar: cat or crow? ")
+if familiar == "cat":
+  print("Good pick!")
+else:
+  print("Very cool.")
+hat = input("Pick a hat: yes hat or no hat? ")
+if hat == "yes hat":
+  print("A witch hat it is!")
+else:
+  print("No, you NEED a hat.")
+school = input("Pick a school: yes school or no school? ")
+if school == "yes school":
+  print("Welcome.")
+else:
+  print("To bad, your going.")
