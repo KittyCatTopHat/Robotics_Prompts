1d611fc8a9f63823eb09b3b5c01286698625dbac
KittyCatTopHat <irenek322@stemcollegiate.ca>
2025-03-07 08:55:00 -0700

    Create mar7.py

---
+++ mar7.py
@@ -0,0 +1,11 @@
+# Today we learned about elif statements. Thaey are now my new favourite things in python, the possibilities are endless!
+Username = input("Enter Username: ")
+Password = input("Enter Password: ")
+if Username == "Irene" and Password == "ic3cr34m":
+  print("Hi me!")
+elif Username == "Hailie" and Password == "R1inett3":
+  print("It's a me, a Grian!")
+elif Username == "Tristan" and Password == "Sp4c3":
+  print("I still want that car.")
+else:
+  print("No.")
